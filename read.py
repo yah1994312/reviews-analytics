f00aeb23a1f9da68d8bc1d46061c59a459a122e3
Gordon Chang <496929145@qq.com>
2020-03-30 11:26:58 +0800

    add filtering

--- read.py
+++ read.py
@@ -13,3 +13,10 @@ for d in data:
 	len(d)
 	sum_len = sum_len + len(d)
 print('留言的平均為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
