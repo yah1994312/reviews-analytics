f7076902aba3dfceeb17fea2b2c1010a75ac7102
Gordon Chang <496929145@qq.com>
2020-04-17 18:10:12 +0800

    add word count function

--- read.py
+++ read.py
@@ -1,22 +1,65 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
 print('檔案讀取完了，一共有', len(data), '筆資料')
+
+print(data[0])
+
 
 sum_len = 0
 for d in data:
-	len(d)
-	sum_len = sum_len + len(d)
+   len(d)
+   sum_len = sum_len + len(d)
 print('留言的平均為', sum_len/len(data))
 
 new = []
 for d in data:
-	if len(d) < 100:
-		new.append(d)
+   if len(d) < 100:
+       new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
+
+good = []
+for d in data:
+   if 'good' in d:
+       good.append(d)
+print('一共有', len(good), '筆留言有good')
+
+good = [d for d in data if 'good' in d]
+bad = ['bad' in d for d in data ] #求出的是布林值
+
+bad = []
+for d in data:
+    bad.append('bad' in d)
+
+#文字計數
+wc = {} #word_count
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1 #新增新的KEY進wc字典
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('請問你想要查什麽字：')
+	if word == 'q':
+		break	
+	if word in wc:
+	    print(word, '出現的次數為：', wc[word], '次')
+	else:
+		print('這個字沒有出現過哦')
+print('感謝使用')
+
